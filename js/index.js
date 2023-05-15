3518a2db690172be77176a2745cc8d4cad010ddc
Rinan <jandrewschnell@gmail.com>
2023-05-15 13:44:45 +0200

    one running line

--- js/index.js
+++ js/index.js
@@ -6,18 +6,58 @@ var ctx = canvas.getContext("2d");
 ctx.font = "25px Arial";
 ctx.textAlign = "center";
 let animation;
+let frames = 0;
 
 //Create canvas
 function drawCanvas(){
+    console.log(frames)
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //highlight numberpads
     if(keys['Numpad3']){
         ctx.beginPath();
-        ctx.fillStyle = "green";
+        ctx.fillStyle = "skyblue";
         ctx.rect(0, 575, 100, 25);
         ctx.fill();}
-
-
+    if(keys['Numpad2']){
+        ctx.beginPath();
+        ctx.fillStyle = "skyblue";
+        ctx.rect(100, 575, 100, 25);
+        ctx.fill();}
+    if(keys['Numpad1']){
+        ctx.beginPath();
+        ctx.fillStyle = "skyblue";
+        ctx.rect(200, 575, 100, 25);
+        ctx.fill();}
+    if(keys['Numpad6']){
+        ctx.beginPath();
+        ctx.fillStyle = "skyblue";
+        ctx.rect(0, 550, 100, 25);
+        ctx.fill();}
+    if(keys['Numpad5']){
+        ctx.beginPath();
+        ctx.fillStyle = "skyblue";
+        ctx.rect(100, 550, 100, 25);
+        ctx.fill();}  
+    if(keys['Numpad4']){
+        ctx.beginPath();
+        ctx.fillStyle = "skyblue";
+        ctx.rect(200, 550, 100, 25);
+        ctx.fill();}  
+    
+    //draw numberbase on canvas
     ctx.fillStyle = "black";
+    ctx.beginPath();
+    ctx.rect(0, 0, 300, 600);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(100, 0);
+    ctx.lineTo(100, 550);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(200, 0);
+    ctx.lineTo(200, 550);
+    ctx.stroke();
     ctx.fillText("3", 50, 595);
     ctx.beginPath();
     ctx.rect(0, 575, 100, 25);
@@ -42,58 +82,45 @@ function drawCanvas(){
     ctx.beginPath();
     ctx.rect(200, 550, 100, 25);
     ctx.stroke();
+    ctx.fillStyle = "green";
+    ctx.beginPath();
+    ctx.rect(0, 515, 300, 35);
+    ctx.fill();
+
+
+    ctx.beginPath();
+    ctx.moveTo(0, frames);
+    ctx.lineTo(300, frames);
+    ctx.stroke();
+    //Running Background on game field
+    if(frames % 120 === 0){
+
+    }
+
+    frames++;
 }
 
 
-//create listener that detects keys presses
+//create listener that detects keys press combination
 document.addEventListener ("keydown", (event) => {
     keys = (keys || []);
         keys[event.code]=true;
     
     if(keys['Space']){
         console.log(keys)
-        //put a function that will check if key combination is the same as the one on place
+//put a function that will check if key combination is the same as the one on place
     }
     if(keys['Numpad3']){
         ctx.beginPath();
         ctx.rect(0, 575, 100, 25);
         ctx.fill();
     }
-    
-    
-    // switch (event.code){
-    //     case 'Numpad3': ctx.beginPath();
-    //                     ctx.rect(0, 575, 100, 25);
-    //                     ctx.fill();
-    //                     break;
-    //     case 'Numpad2': ctx.beginPath();
-    //                     ctx.rect(100, 575, 100, 25);
-    //                     ctx.fill();
-    //                     break;
-    //     case 'Numpad1': ctx.beginPath();
-    //                     ctx.rect(200, 575, 100, 25);
-    //                     ctx.fill();
-    //                     break;
-    //     case 'Numpad6': ctx.beginPath();
-    //                     ctx.rect(0, 550, 100, 25);
-    //                     ctx.fill();
-    //                     break;
-    //     case 'Numpad5': ctx.beginPath();
-    //                     ctx.rect(100, 550, 100, 25);
-    //                     ctx.fill();
-    //                     break;
-    //     case 'Numpad4': ctx.beginPath();
-    //                     ctx.rect(200, 550, 100, 25);
-    //                     ctx.fill();
-    //                     break;
-    // }
 }, false);
 
 document.addEventListener("keyup", function (e) {
     keys[e.code]=false;
     stop();
 }, false);
-//create a combination listener when space is pressed
 
 
 
@@ -103,4 +130,4 @@ function game(){
     animation = requestAnimationFrame(game)
 }
 
-game()
+game();
