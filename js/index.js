90fe463994a4560bee675d30108c7ce9a529c937
Rinan <jandrewschnell@gmail.com>
2023-05-15 11:21:35 +0200

    color highlight number 3

--- js/index.js
+++ js/index.js
@@ -1,61 +1,106 @@
-//Create canvas
+//initial values
+let keys=[];
+let test = 'Numpad1';
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 ctx.font = "25px Arial";
 ctx.textAlign = "center";
-ctx.fillText("3", 50, 595);
-ctx.beginPath();
-ctx.rect(0, 575, 100, 25);
-ctx.stroke();
-ctx.fillText("2", 150, 595);
-ctx.beginPath();
-ctx.rect(100, 575, 100, 25);
-ctx.stroke();
-ctx.fillText("1", 250, 595);
-ctx.beginPath();
-ctx.rect(200, 575, 100, 25);
-ctx.stroke();
-ctx.fillText("6", 50, 570);
-ctx.beginPath();
-ctx.rect(0, 550, 100, 25);
-ctx.stroke();
-ctx.fillText("5", 150, 570);
-ctx.beginPath();
-ctx.rect(100, 550, 100, 25);
-ctx.stroke();
-ctx.fillText("4", 250, 570);
-ctx.beginPath();
-ctx.rect(200, 550, 100, 25);
-ctx.stroke();
+let animation;
+
+//Create canvas
+function drawCanvas(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if(keys['Numpad3']){
+        ctx.beginPath();
+        ctx.fillStyle = "green";
+        ctx.rect(0, 575, 100, 25);
+        ctx.fill();}
+
+
+    ctx.fillStyle = "black";
+    ctx.fillText("3", 50, 595);
+    ctx.beginPath();
+    ctx.rect(0, 575, 100, 25);
+    ctx.stroke();
+    ctx.fillText("2", 150, 595);
+    ctx.beginPath();
+    ctx.rect(100, 575, 100, 25);
+    ctx.stroke();
+    ctx.fillText("1", 250, 595);
+    ctx.beginPath();
+    ctx.rect(200, 575, 100, 25);
+    ctx.stroke();
+    ctx.fillText("6", 50, 570);
+    ctx.beginPath();
+    ctx.rect(0, 550, 100, 25);
+    ctx.stroke();
+    ctx.fillText("5", 150, 570);
+    ctx.beginPath();
+    ctx.rect(100, 550, 100, 25);
+    ctx.stroke();
+    ctx.fillText("4", 250, 570);
+    ctx.beginPath();
+    ctx.rect(200, 550, 100, 25);
+    ctx.stroke();
+}
 
 
 //create listener that detects keys presses
 document.addEventListener ("keydown", (event) => {
-    switch (event.code){
-        case 'Numpad3': ctx.beginPath();
-                        ctx.rect(0, 575, 100, 25);
-                        ctx.fill();
-                        break;
-        case 'Numpad2': ctx.beginPath();
-                        ctx.rect(100, 575, 100, 25);
-                        ctx.fill();
-                        break;
-        case 'Numpad1': ctx.beginPath();
-                        ctx.rect(200, 575, 100, 25);
-                        ctx.fill();
-                        break;
-        case 'Numpad6': ctx.beginPath();
-                        ctx.rect(0, 550, 100, 25);
-                        ctx.fill();
-                        break;
-        case 'Numpad5': ctx.beginPath();
-                        ctx.rect(100, 550, 100, 25);
-                        ctx.fill();
-                        break;
-        case 'Numpad4': ctx.beginPath();
-                        ctx.rect(200, 550, 100, 25);
-                        ctx.fill();
-                        break;
+    keys = (keys || []);
+        keys[event.code]=true;
+    
+    if(keys['Space']){
+        console.log(keys)
+        //put a function that will check if key combination is the same as the one on place
     }
-})
+    if(keys['Numpad3']){
+        ctx.beginPath();
+        ctx.rect(0, 575, 100, 25);
+        ctx.fill();
+    }
+    
+    
+    // switch (event.code){
+    //     case 'Numpad3': ctx.beginPath();
+    //                     ctx.rect(0, 575, 100, 25);
+    //                     ctx.fill();
+    //                     break;
+    //     case 'Numpad2': ctx.beginPath();
+    //                     ctx.rect(100, 575, 100, 25);
+    //                     ctx.fill();
+    //                     break;
+    //     case 'Numpad1': ctx.beginPath();
+    //                     ctx.rect(200, 575, 100, 25);
+    //                     ctx.fill();
+    //                     break;
+    //     case 'Numpad6': ctx.beginPath();
+    //                     ctx.rect(0, 550, 100, 25);
+    //                     ctx.fill();
+    //                     break;
+    //     case 'Numpad5': ctx.beginPath();
+    //                     ctx.rect(100, 550, 100, 25);
+    //                     ctx.fill();
+    //                     break;
+    //     case 'Numpad4': ctx.beginPath();
+    //                     ctx.rect(200, 550, 100, 25);
+    //                     ctx.fill();
+    //                     break;
+    // }
+}, false);
+
+document.addEventListener("keyup", function (e) {
+    keys[e.code]=false;
+    stop();
+}, false);
 //create a combination listener when space is pressed
+
+
+
+//game function
+function game(){
+    drawCanvas();
+    animation = requestAnimationFrame(game)
+}
+
+game()
